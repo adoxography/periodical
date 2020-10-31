12696e48a04069db216b50df5b858d81495970a0
adoxography <nevermore667@gmail.com>
2020-10-31 15:10:12 -0500

    Add eslint

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,10 @@
 require('./bootstrap');
-
 const Turbolinks = require('turbolinks');
+
 Turbolinks.start();
 Turbolinks.setProgressBarDelay(0);
 
-document.addEventListener('turbolinks:load', evt => {
+document.addEventListener('turbolinks:load', () => {
   document.querySelectorAll('#main-content img').forEach(img => {
     img.classList.add('animate__animated', 'animate__fadeIn');
   });
