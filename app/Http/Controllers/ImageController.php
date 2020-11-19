63143efc0f3b5b2ea7f39b20e37ca51ac0de66ca
adoxography <nevermore667@gmail.com>
2020-11-18 22:38:07 -0600

    Add return typehint

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ImageController extends Controller
 {
-    public function show(string $id)
+    public function show(string $id): StreamedResponse
     {
         return Storage::response("/images/$id");
     }
